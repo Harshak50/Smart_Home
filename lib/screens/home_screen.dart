64409c5c0af9fa6ae9759d70d5b583ab37d09459
Harsha <konndapuramharsha@gmail.com>
2022-01-07 19:27:47 +0530

    Added Cards

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_switch/flutter_switch.dart';
 import 'package:smart_home/utils/constants.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,6 +11,8 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
+  bool status = false;
+  bool status1 = false; 
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
@@ -60,7 +63,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         shape:
                             MaterialStateProperty.all<RoundedRectangleBorder>(
                                 RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(30.0),
+                                    borderRadius: BorderRadius.circular(20.0),
                                     side: BorderSide(color: Colors.white54))),
                         backgroundColor:
                             MaterialStateProperty.all(Color(0xff898380))),
@@ -88,43 +91,271 @@ class _HomeScreenState extends State<HomeScreen> {
                   fontSize: 20),
             ),
           ),
+
+
+          //  Home Temp and Plug Wall Cards
           Positioned(
-              top: size.height * 0.23,
-              left: 7,
-              child: Row(
-                children: [
-                  Card(
-                    color: kCardColor,
+            top: size.height * 0.23,
+            left: 7,
+            child: Row(
+              children: [
+                Card(
+                  elevation: 2,
+                  color: kCardColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 40, horizontal: 22),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Home",
+                          style: TextStyle(
+                              color: kDarkColor,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        Text(
+                          "Temperature",
+                          style: TextStyle(
+                              color: kDarkColor,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Text(
+                              "23",
+                              style: TextStyle(
+                                  color: kDarkColor,
+                                  fontSize: 40,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                            Text(
+                              " °C",
+                              style: TextStyle(
+                                  color: kDarkColor,
+                                  fontSize: 26,
+                                  fontWeight: FontWeight.w400),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 2),
+                        FlutterSwitch(
+                          width: 55.0,
+                          height: 30.0,
+                          toggleSize: 24.0,
+                          activeColor: kChipColor,
+                          inactiveColor: kGreyColor,
+                          value: status,
+                          borderRadius: 30.0,
+                          padding: 0.5,
+                          onToggle: (val) {
+                            setState(() {
+                              status = val;
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(width: size.width*0.08,),
+                Container(
+                  child: Card(
+                    color: Colors.white30,
+                    elevation: 2,
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(20),
                     ),
                     child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 40,horizontal: 10),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 30, horizontal: 18),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.start,
                         children: [
-                          Text(
-                            "Home",
-                            style: TextStyle(color: kDarkColor, fontSize: 18,fontWeight: FontWeight.w600),
-                          ),
-                          Text("Temperature",style: TextStyle(color: kDarkColor, fontSize: 18,fontWeight: FontWeight.w600),
-                          ),
-                          SizedBox(height:5),
                           Row(
                             children: [
-                              
-                              Text("23 °C",style: TextStyle(color: kDarkColor, fontSize: 40,fontWeight: FontWeight.w600),
+                              Text(
+                                "   Plug Wall ",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w600),
                               ),
+                              Icon(Icons.arrow_forward_ios, color: Colors.white,size: 15,)
                             ],
                           ),
-                          
+                          SizedBox(height: 8),
+                          Text(
+                            "\u2022 MacBook Pro",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w400),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            "\u2022 HomePod",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w400),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            "\u2022 PlayStation 5",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w400),
+                          ),
+                          SizedBox(height: 10),
+                          FlutterSwitch(
+                            width: 55.0,
+                            height: 30.0,
+                            toggleSize: 24.0,
+                            activeColor: kChipColor,
+                            inactiveColor: Colors.white54,
+                            value: status1,
+                            borderRadius: 30.0,
+                            padding: 0.7,
+                            onToggle: (val) {
+                              setState(() {
+                                status = val;
+                              });
+                            },
+                          ),
                         ],
                       ),
                     ),
                   ),
-                ],
-              ))
+                ),
+              ],
+            ),
+          ),
+
+//  play and smart tv cards
+
+                Positioned(
+                  top: size.height * 0.5,
+                   left: 7,
+                  child: Row(
+                    children: [
+                     
+                      Container(
+                        child: Card(
+                          color: Colors.white30,
+                          elevation: 2,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 30, horizontal: 10),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    CircleAvatar(
+                                      radius: 25,
+                                      backgroundImage:NetworkImage("https://images.unsplash.com/photo-1505282722405-413748d3de7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80"),
+                                    ),
+                                    Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          " Midnight Love",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.w500),
+                                        ),
+                                        Text(
+                                          " Girl in red",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 12,
+                                              fontWeight: FontWeight.w500),
+                                        ),
+                                      ],
+                                    ),
+                                   
+                                  ],
+                                ),
+                                SizedBox(height: 8),
+                                
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                       Container(
+                        child: Card(
+                          color: Colors.white30,
+                          elevation: 2,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 30, horizontal: 18),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    Text(
+                                      "   Smart TV  ",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                    Icon(Icons.arrow_forward_ios, color: Colors.white,size: 15,)
+                                  ],
+                                ),
+                                SizedBox(height: 8),
+                                Text(
+                                  "\u2022 Samsung UA55 4AC",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w400),
+                                ),
+                                SizedBox(height: 8),
+                                SizedBox(height: 10),
+                                FlutterSwitch(
+                                  width: 55.0,
+                                  height: 30.0,
+                                  toggleSize: 24.0,
+                                  activeColor: kChipColor,
+                                  inactiveColor: Colors.white30,
+                                  value: status1,
+                                  borderRadius: 30.0,
+                                  padding: 0.5,
+                                  onToggle: (val) {
+                                    setState(() {
+                                      status = val;
+                                    });
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+        
+        
         ],
       ),
     );
